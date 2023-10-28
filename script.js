f97580102a7ea8cc5428ef169ec307f44514e2b0
filippo <riccardo.gabba02@gmail.com>
2023-10-28 10:54:31 +0200

    i can't solve the problem

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 simonSays();
+
 function simonSays (){
     const col = document.createElement('div');
-    
     const min = 1
-    const max = 100
-
+    const max = 10
     const numbersLenght = 5 
-
     const numeriDaIndovinare = []
-
-    const timer = 3000 
+    const timer = 3000
+    
+    const numbers = document.getElementById('numbers')
+    numbers.append(col)
+    
+    for(let i = 0; i < 5; i++){
+        numeriDaIndovinare.push
+    }
 }
 
-function creazioneNumeri(){
-    let numbers = Math.floor((Math.random() * 100) + 1);
-    document.getElementById("numbers").innerHTML
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+

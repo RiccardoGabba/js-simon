d60662fc64561bff6cbde3973110600c1213a90e
filippo <riccardo.gabba02@gmail.com>
2023-10-28 10:25:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+simonSays();
+function simonSays (){
+    const col = document.createElement('div');
+    
+    const min = 1
+    const max = 100
+
+    const numbersLenght = 5 
+
+    const numeriDaIndovinare = []
+
+    const timer = 3000 
+}
